104dc1dae1275052e31b64b0e63b4b9b0c1d344a
Vanessa Sarrias <kerlysarrias011@gmail.com>
2024-02-15 23:16:42 -0500

    darkmode changes

--- assets/js/helpers/theme.js
+++ assets/js/helpers/theme.js
@@ -7,13 +7,18 @@ function changeTheme(e) {
     const styleRoot = document.documentElement.style;
     if(checked) {
         styleRoot.setProperty('--bg-color-section1', '#0d1b2a');
+        styleRoot.setProperty('--bg-color-skills', '#343a40');
         styleRoot.setProperty('--color-black', '#eee');
         styleRoot.setProperty('--bg-color-section2', '#0d1b2a');
+        styleRoot.setProperty('--card', '#343a40');
+        
         
     } else {
         styleRoot.setProperty('--bg-color-section1', '#eee');
         styleRoot.setProperty('--color-black', '#000');
         styleRoot.setProperty('--bg-color-section2', '#fafafa');
+        styleRoot.setProperty('--bg-color-skills', '#d1d1d1');
+        styleRoot.setProperty('--card', '#d1d1d1');
     }
 }
 
